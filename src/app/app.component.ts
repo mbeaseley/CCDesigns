302168b7f67eac7a51c6f38831fdeb24d4b2a7a1
Michael Beaseley <michael.beaseley@MAC000353.local>
2018-02-11 21:01:53 +0000

    integration: updated the layout of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  @Input()
+  click: boolean;
+  classId: any;
+  title: any;
+
+  constructor() {
+    this.title = 'app';
+    this.click = false;
+  }
+
+  ngOnInit() {
+
+  }
+
+  myMethod() {
+    if (this.click === false) {
+      this.click = true;
+      console.log('click_change: ', this.click);
+
+      
+      $('page__socialContrainer').addClass('socialContrainerMove');
+      return console.log('testy');
+    } else {
+      this.click = false;
+      console.log('click_change: ', this.click);
+    }
+  }
 }
