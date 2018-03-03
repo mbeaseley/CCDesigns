2fc449e09b342b42420e5af0df0b4226471f3e81
Michael Beaseley <michael.beaseley@MAC000353.local>
2018-03-03 00:19:35 +0000

    navbar solution to force navbar to not body down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,6 @@ import * as $ from 'jquery';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  @Input()
-  click: boolean;
-  classId: any;
-  title: any;
-
-  constructor() {
-    this.title = 'app';
-    this.click = false;
-  }
-
-  ngOnInit() {
-
-  }
-
-  myMethod() {
-    if (this.click === false) {
-      this.click = true;
-      console.log('click_change: ', this.click);
-
-      
-      $('page__socialContrainer').addClass('socialContrainerMove');
-      return console.log('testy');
-    } else {
-      this.click = false;
-      console.log('click_change: ', this.click);
-    }
-  }
+export class AppComponent {
+  constructor() {}
 }
