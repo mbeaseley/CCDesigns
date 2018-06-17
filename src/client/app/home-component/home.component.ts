95a1173ca9dc399035219b42b28a3d3cc2fc2d6f
mbeaseley <m.t.beaseley@gmail.com>
2018-06-17 20:22:49 +0100

    ,master: remove useless dependencies on repo

--- src/client/app/home-component/home.component.ts
+++ src/client/app/home-component/home.component.ts
@@ -9,10 +9,7 @@ import { SlideshowModule } from 'ng-simple-slideshow';
 })
 export class HomeComponent implements OnInit {
   imageUrls = [
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg' },
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg' },
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg' },
-    { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56674755/mr_pb_is_the_best.0.jpg' },
+    
   ];
 
   ngOnInit() {}
